414e2a58e4fb6a707047ddecc58ba4d19f8bd099
usorfaitheloho <usorfaitheloho@gmail.com>
2022-10-31 12:24:42 -0700

    Add global style

---
+++ src/component/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../css/homepage.module';
+
+const HomePage = () => {
+  return (
+    <main className={styles.wrapper}>HomePage</main>
+  )
+}
+
+export default HomePage
