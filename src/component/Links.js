3689a7df5105b6b9b4d8ab137c2e179adc8ed558
usorfaitheloho <usorfaitheloho@gmail.com>
2022-10-31 19:57:46 -0700

    add com[pnents

---
+++ src/component/Links.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Links = ({ link, children }) => (
+    <div>
+      <Link to={link}>
+        {children}
+      </Link>
+    </div>
+  );
+
+  Links.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+    link: PropTypes.string.isRequired,
+  };
+
+export default Links;
